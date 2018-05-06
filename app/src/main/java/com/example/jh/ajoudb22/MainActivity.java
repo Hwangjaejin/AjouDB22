0ce8a99e46a9d0965096d18eacbf07f7f19a4608
jaejin <jaejin94@ajou.ac.kr>
2018-05-06 21:15:23 +0900

    회원가입화면 & 화면전환

--- app/src/main/java/com/example/jh/ajoudb22/MainActivity.java
+++ app/src/main/java/com/example/jh/ajoudb22/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jh.ajoudb22;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
+
     }
 }
