003825dcada566184832f637a14ca3b7ac1bf47e
JungWoo <okjwoo@ajou.ac.kr>
2018-06-02 19:20:10 +0900

    지도 리뷰 toolbar 추가

--- app/src/main/java/com/example/jh/ajoudb22/MapActivity.java
+++ app/src/main/java/com/example/jh/ajoudb22/MapActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,15 +33,28 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static float lon = 0;
     private static String r_name;
 
+    private Toolbar toolbar;
+    private TextView Toolbar_Text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        Toolbar_Text=(TextView)findViewById(R.id.Toolbar_text);
         Intent intent = getIntent();
         r_name = intent.getStringExtra("Name");
         lat = Float.parseFloat(intent.getStringExtra("Latitude"));
         lon = Float.parseFloat(intent.getStringExtra("Longitude"));
+
+        Toolbar_Text.setText(r_name);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
