4e8cff66be6a8eb40d0ad3a815379e5b9e2b0f20
seung3063 <daekwan6@naver.com>
2018-06-02 20:12:34 +0900

    레스토랑 리스트 and Fragment

--- app/src/main/java/com/example/jh/ajoudb22/RestaurantActivity.java
+++ app/src/main/java/com/example/jh/ajoudb22/RestaurantActivity.java
@@ -1,6 +1,9 @@
 package com.example.jh.ajoudb22;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -118,6 +121,9 @@ public class RestaurantActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
         //actionBar.setHomeAsUpIndicator(R.drawable.button_back);
+
+        Drawable drawable=ratingBar.getProgressDrawable();
+        drawable.setColorFilter(Color.parseColor("#F9C835"), PorterDuff.Mode.SRC_ATOP);
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://jaejindb.cafe24.com/RestaurantInfo.php";
